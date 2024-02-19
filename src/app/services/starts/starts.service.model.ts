f8408b8bfde440be65d81c7a7beb187ede43c070
thelscon <thelscon@gmail.com>
2024-02-19 19:55:10 +0200

    correct

--- src/app/services/starts/starts.service.model.ts
+++ src/app/services/starts/starts.service.model.ts
@@ -1,5 +1,7 @@
 export interface StartsServiceModel {
     spaceTitle : string
     title : string
+    content : string
+    button : string
     plantTitle : string
 }
--- src/app/services/starts/starts.service.ts
+++ src/app/services/starts/starts.service.ts
@@ -8,5 +8,7 @@ import { StartsServiceModel } from './starts.service.model';
 export class StartsService implements StartsServiceModel {
   spaceTitle = 'Wellbeing starts with well doing'
   title = 'Wellbeing starts with welldoing'
+  content = 'Lorem ipsum dolor sit amet consectetur. Quam ut consequat at a. Diam lacus platea orci vel elit blandit facilisis enim risus. Ut tristique eget integer odio nec.'
+  button = 'Join now'
   plantTitle = 'Plant Protective'
 }
